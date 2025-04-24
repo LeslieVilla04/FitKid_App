47ef2cab82d2649f299d1a3095d10221a3d0106e
Leslie <leslievillalta1010@gmail.com>
2025-04-23 23:25:48 -0700

    Initial Commit

---
+++ FitKidApp/FitKidAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitKidAppApp.swift
+//  FitKidApp
+//
+//  Created by Leslie Villalta on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct FitKidAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
